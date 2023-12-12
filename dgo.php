b33357c9aba03d41cd7fd4a021c30cf735201f95
cryptolash <107434542+cryptolash@users.noreply.github.com>
2023-12-11 21:02:36 -0400

    Create dgo.php

---
+++ dgo.php
@@ -0,0 +1,99 @@
+
+<html lang="es">
+<head>
+
+<meta name=robots content=noindex />
+<style>body{margin:0;padding:0}</style>
+<script src="//ssl.p.jwpcdn.com/player/v/8.26.0/jwplayer.js"></script>
+<script> jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';
+</script>
+<div id="player"></div>
+<script>
+    let token = 'eyJ0eXAiOiJKV1QiLCJraWQiOiJ3VTNpZklJYUxPVUFSZVJCL0ZHNmVNMVAxUU09IiwiYWxnIjoiUlMyNTYifQ.eyJhdF9oYXNoIjoic25BSW9wemxpMkNKc3l5enllXzZIdyIsInN1YiI6InNhbmRyYW1vcnZhbEBob3RtYWlsLmNvbSIsImJ1c2luZXNzVW5pdCI6IlNTTEEtRFRIIiwicmVnaW9ucyI6WyJmOTE3Y2ZmNi0wMjVhLTQ5MTctODRkNi1mYjY4NjY5N2Q5M2MiLCIxMmY0ZjBlYi0zODdlLTRmZjMtYjhkMC1hMzBjNzdhY2ZlNzQiXSwiYXVkaXRUcmFja2luZ0lkIjoiMzNiZDJhMzgtMTQ4ZC00ZjY1LTliZTgtZTE0MDk4Y2E5M2ExLTExMTYzMzUyIiwiaXNvMkNvZGUiOiJDTyIsImlzcyI6Imh0dHBzOi8vcHJvZHJvY2suZGlyZWN0dmdvLmNvbTo0NDMvb3BlbmFtL29hdXRoMi9yZWFsbXMvcm9vdC9yZWFsbXMvc3NsYSIsInRva2VuTmFtZSI6ImlkX3Rva2VuIiwiZGV2aWNlSWQiOiJ6VTBlQmdqVUtTU1owTEN6YzFsbVloSTQ0QWlzWjNIQXJfNUFwZzFJYVZRIiwiY3VzdG9tZXJTdGF0dXMiOiJBY3RpdmUiLCJhenAiOiJkdHZnbyIsImZhbWlseU5hbWUiOiJNT1JFTk8gVkFMRU5DSUEiLCJhdXRoX3RpbWUiOjE3MDIxODQ5MDQsImN1c3RvbWVySWQiOiI4NDA0NDkyMCIsImV4cCI6MTcwMjM1MDUwNywiaWF0IjoxNzAyMTg0OTA3LCJjdXN0b21lclN0YXR1c01lc3NhZ2UiOiIxMDAxIiwiaWJzQWNjb3VudFN0YXR1cyI6IkFDVElWRSIsImdpdmVuTmFtZSI6IlNBTkRSQSIsInNlcnZpY2VQcm92aWRlckFjY291bnRJZCI6IjhlMTgwNWU0LTZlNTctNDRkZi05ODE1LTY5ZDliMWFiMDRmNCIsInZyaW9EZXZpY2VJZCI6ImNmMGMzOTRlLWVmZGUtNGFmMS04MzYxLTY1MDE5MDAwYTc2NiIsImF1ZCI6ImR0dmdvIiwiQ0IxIjpmYWxzZSwiY2hhbm5lbHMiOlsiQ0gwMTAwMDAwMDAwMDE5IiwiQ0gwMTAwMDAwMDAwMTY3IiwiQ0gwMTAwMDAwMDAwMTk1IiwiQ0gwMTAwMDAwMDAwMDc2IiwiQ0gwMTAwMDAwMDAwMTA3IiwiQ0gwMTAwMDAwMDAwMjAyIiwiQ0gwMTAwMDAwMDAwMDI2IiwiQ0gwMTAwMDAwMDAwMTY5IiwiQ0gwMTAwMDAwMDAwMTUxIiwiQ0gwMTAwMDAwMDAwMjE2IiwiQ0gwMTAwMDAwMDAwMjA5IiwiQ0gwMTAwMDAwMDAwMDc1IiwiQ0gwMTAwMDAwMDAwMjk1IiwiQ0gwMTAwMDAwMDAwMjkzIiwiQ0gwMTAwMDAwMDAwMDM5IiwiQ0gwMTAwMDAwMDAwMDg3IiwiQ0gwMTAwMDAwMDAwMjI5IiwiQ0gwMTAwMDAwMDAwMDE4IiwiQ0gwMTAwMDAwMDAwMDY4IiwiQ0gwMTAwMDAwMDAwMDM4IiwiQ0gwMTAwMDAwMDAwMDIwIiwiQ0gwMTAwMDAwMDAwMDE1IiwiQ0gwMTAwMDAwMDAwMTQyIiwiQ0gwMTAwMDAwMDAwMTQwIiwiQ0gwMTAwMDAwMDAwMTcxIiwiQ0gwMTAwMDAwMDAwMTE3IiwiQ0gwMTAwMDAwMDAwMDY1IiwiQ0gwMTAwMDAwMDAwMDQ2IiwiQ0gwMTAwMDAwMDAwMjM1IiwiQ0gwMTAwMDAwMDAwMjg2IiwiQ0gwMTAwMDAwMDAwMjkyIiwiQ0gwMTAwMDAwMDAwMTM5IiwiQ0gwMTAwMDAwMDAwMjAzIiwiQ0gwMTAwMDAwMDAwMDUwIiwiQ0gwMTAwMDAwMDAwMDc0IiwiQ0gwMTAwMDAwMDAwMTE4IiwiQ0gwMTAwMDAwMDAwMTU2IiwiQ0gwMTAwMDAwMDAwMTYwIiwiQ0gwMTAwMDAwMDAwMTYxIiwiQ0gwMTAwMDAwMDAwMTc1IiwiQ0gwMTAwMDAwMDAwMDE3IiwiQ0gwMTAwMDAwMDAwMDk2IiwiQ0gwMTAwMDAwMDAwMTgxIiwiQ0gwMTAwMDAwMDAwMTY1IiwiQ0gwMTAwMDAwMDAwMDgwIiwiQ0gwMTAwMDAwMDAwMDY0IiwiQ0gwMTAwMDAwMDAwMDMyIiwiQ0gwMTAwMDAwMDAwMDg2IiwiQ0gwMTAwMDAwMDAwMDk1IiwiQ0gwMTAwMDAwMDAwMTA0IiwiQ0gwMTAwMDAwMDAwMTQ4IiwiQ0gwMTAwMDAwMDAwMTA5IiwiQ0gwMTAwMDAwMDAwMTg2IiwiQ0gwMTAwMDAwMDAwMjE1IiwiQ0gwMTAwMDAwMDAwMjIzIiwiQ0gwMTAwMDAwMDAwMjQ3IiwiQ0gwMTAwMDAwMDAwMjUyIiwiQ0gwMTAwMDAwMDAwMjU5IiwiQ0gwMTAwMDAwMDAwMTk2IiwiQ0gwMTAwMDAwMDAwMjAxIiwiQ0gwMTAwMDAwMDAwMTI1IiwiQ0gwMTAwMDAwMDAwMTI4IiwiQ0gwMTAwMDAwMDAwMTk3IiwiQ0gwMTAwMDAwMDAwMDM3IiwiQ0gwMTAwMDAwMDAwMjM2IiwiQ0gwMTAwMDAwMDAwMjQ4IiwiQ0gwMTAwMDAwMDAwMTQ2IiwiQ0gwMTAwMDAwMDAwMTE1IiwiQ0gwMTAwMDAwMDAwMTk0IiwiQ0gwMTAwMDAwMDAwMjYzIiwiQ0gwMTAwMDAwMDAwMDk4IiwiQ0gwMTAwMDAwMDAwMjA3IiwiQ0gwMTAwMDAwMDAwMDUzIiwiQ0gwMTAwMDAwMDAwMjY1IiwiQ0gwMTAwMDAwMDAwMjY2IiwiQ0gwMTAwMDAwMDAwMDQ0IiwiQ0gwMTAwMDAwMDAwMDUyIiwiQ0gwMTAwMDAwMDAwMjQxIiwiQ0gwMTAwMDAwMDAwMTg0IiwiQ0gwMTAwMDAwMDAwMjIyIiwiQ0gwMTAwMDAwMDAwMjM5IiwiQ0gwMTAwMDAwMDAwMDg1IiwiQ0gwMTAwMDAwMDAwMTkzIiwiQ0gwMTAwMDAwMDAwMTA2IiwiQ0gwMTAwMDAwMDAwMTExIiwiQ0gwMTAwMDAwMDAwMTEyIiwiQ0gwMTAwMDAwMDAwMTUzIiwiQ0gwMTAwMDAwMDAwMTE5IiwiQ0gwMTAwMDAwMDAwMzAyIiwiQ0gwMTAwMDAwMDAwMTQzIiwiQ0gwMTAwMDAwMDAwMjgxIiwiQ0gwMTAwMDAwMDAwMjMyIiwiQ0gwMTAwMDAwMDAwMjgzIiwiQ0gwMTAwMDAwMDAwMjk4IiwiQ0gwMTAwMDAwMDAwMjA4IiwiQ0gwMTAwMDAwMDAwMjI2IiwiQ0gwMTAwMDAwMDAwMjMzIiwiQ0gwMTAwMDAwMDAwMjc2IiwiQ0gwMTAwMDAwMDAwMzEyIiwiQ0gwMTAwMDAwMDAwMzE1IiwiQ0gwMTAwMDAwMDAwMzE3IiwiQ0gwMTAwMDAwMDAwMzI5IiwiQ0gwMTAwMDAwMDAwMzMxIiwiQ0gwMTAwMDAwMDAwMzM0IiwiQ0gwMTAwMDAwMDAwMzM3IiwiQ0gwMTAwMDAwMDAwMzQyIiwiQ0gwMTAwMDAwMDAwMzQwIiwiQ0gwMTAwMDAwMDAwMzQxIiwiQ0gwMTAwMDAwMDAwMzQzIl0sInZyaW9DdXN0b21lcklkIjoiOGUxODA1ZTQtNmU1Ny00NGRmLTk4MTUtNjlkOWIxYWIwNGY0IiwibmFtZSI6IlNBTkRSQSBNT1JFTk8gVkFMRU5DSUEiLCJyZWFsbSI6Ii9zc2xhIiwiY3NtIjoiMiIsInRva2VuVHlwZSI6IkpXVFRva2VuIn0.JcE-M4wQAenjgjV516EZ5Hp-xSCPVmm2VWi06ZQ-qbjuNftImvlCFjXUKlHUTulAxJ0bnrgFx39wGpahb35SFUrS_FzWoXkTOHDnDpwqqaWXtAvNfXjIJB8pSprXdQHx4UsjAJUIA-e945RmbIE0OrqeHWCJ7v5tj-pZU2R7AwcdVZ9tbs9IaQWCYzuxCZSyfLTCIpwlkTO9utZVQzsf0N7D7UHaEp1HHuB9Jz5evHaxVLyWaitTP5gSLSk6BzrLnt_uZeqQWECPG_uOAEp9KH8uqD6QECqQC-_8Ho-VP6CiI8cE97_RbZzjObsIvDHPzhjKrh4jfmMph27511cU0Q';  // Coloca tu token aquí
+    const urlParams = new URLSearchParams(window.location.search);
+    let chid = urlParams.get('id');
+    let keyId = urlParams.get('key');
+    let key = urlParams.get('key2');
+
+    const headers = {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json',
+      'X-Client-Id': 'web',
+      'X-Client-Version': '3.17.0',
+    };
+
+    const body = JSON.stringify({
+      contentId: chid,
+      delay: 0,
+      isLive: true,
+      mobileNetwork: true,
+    });
+
+    fetch('https://api.directvgo.com/entitlement/v3/playback/token/authorizer', {
+      method: 'POST',
+      headers: headers,
+      body: body,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.manifest && data.manifest.dash && data.manifest.dash.primary) {
+          const dashUrl = data.manifest.dash.primary;
+
+          fetch(dashUrl)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error('HTTP error! status: ' + response.status);
+              }
+              const hdntl = response.headers.get('hdntl');
+
+              if (hdntl) {
+                console.log('hdntl:', hdntl);
+
+                const playlist = [
+                  {
+                    title: 'Hus Sports',
+                    description: 'hussports.com',
+                    sources: [
+                      {
+                        default: false,
+                        type: 'dash',
+                        file: dashUrl,
+                        drm: {
+                          clearkey: {
+                            keyId: keyId,
+                            key: key,
+                          },
+                        },
+                        label: '0',
+                        onXhrOpen: function (xhr, url) {
+                          if (!url.includes('manifest.mpd')) {
+                            xhr.setRequestHeader('Hdntl', hdntl);
+                          }
+                        },
+                      },
+                    ],
+                  },
+                ];
+
+                const player = jwplayer('player');
+                player.setup({
+                  playlist: playlist,
+                  width: '100%',
+                  height: '100%',
+                  autostart: false,
+                  cast: false,
+                  sharing: false,
+                });
+              } else {
+                console.log('Header "hdntl" not found in the response.');
+              }
+              return response.text();
+            })
+            .catch((error) => console.error('Error fetching MPD:', error));
+        } else {
+          console.error('Dash primary URL not found in the response.');
+        }
+      })
+      .catch((error) => console.error('Error fetching authorization token:', error));
+</script>
+</html>

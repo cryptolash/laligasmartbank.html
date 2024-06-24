703221c216fd732ead9f0de14ca1f823a78eada3
cryptolash <107434542+cryptolash@users.noreply.github.com>
2024-06-24 15:34:40 -0400

    Update dgo.php

--- dgo.php
+++ dgo.php
@@ -1,1 +1,52 @@
 
+
+<meta name="robots" content="noindex" /> 
+<script src="//cdn.jsdelivr.net/npm/@clappr/player@0.4.0/dist/clappr.min.js"></script> 
+<script src="//cdn.jsdelivr.net/npm/mux.js@5.6.7/dist/mux.min.js"></script> 
+<script src='//cdn.jsdelivr.net/npm/level-selector@latest/dist/level-selector.min.js'></script> 
+<script src='//cdn.jsdelivr.net/npm/clappr-chromecast-plugin@latest/dist/clappr-chromecast-plugin.min.js'></script> 
+<script src='//cdn.jsdelivr.net/npm/clappr-pip@latest/dist/clappr-pip.min.js'></script> 
+<script src='//cdn.jsdelivr.net/npm/clappr-playback-rate-plugin@latest/dist/clappr-playback-rate-plugin.min.js'></script> 
+<script src="//cdn.jsdelivr.net/npm/shaka-player@2.5.10/dist/shaka-player.compiled.min.js"></script> 
+<script src="//cdn.jsdelivr.net/gh/clappr/dash-shaka-playback@latest/dist/dash-shaka-playback.external.min.js"></script> 
+<script src="//cdn.jsdelivr.net/npm/cdnbye-shaka@latest"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>  
+<style>  
+ 
+.player-poster[data-poster] { 
+  background-size: cover; 
+} 
+body{ 
+  margin:0; 
+  padding:0; 
+} 
+ 
+</style> 
+</head> 
+<div id="player"></div> 
+<script> 
+var player = new Clappr.Player({ 
+        source: 'https://tvup-ccs-live.akamaized.net/espn/espn.mpd', 
+        mimeType: 'application/dash+xml', 
+        height: '100%', 
+        width: '100%', 
+        plugins: [LevelSelector, DashShakaPlayback, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, Clappr.MediaControl], 
+        events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},}, 
+        chromecast: { 
+            appId: '9DFB77C0', 
+            contentType: 'video/mp4', 
+        }, 
+        shakaConfiguration: { 
+            drm: { 
+            clearKeys: { 
+                    'cdbfd97ad15c5a68afa552dc73e319b3': '8f91fbee48a48bfaf6fcfef9c19fac12', 
+                    'c590f6c34218580d882b08b90d997676': '4a873136d2e6677ccb306c7dab94821f', 
+                    '1a8c90d674ff513fb4c87d102de5ccba': 'a1b62aef4dae3c95bb8398278d78df5f'
+                } 
+            }, 
+            preferredAudioLanguage: 'pt-BR', 
+        }, 
+        shakaOnBeforeLoad: function (shaka_player) {}, 
+        parentId: '#player' 
+    }); 
+</script>
